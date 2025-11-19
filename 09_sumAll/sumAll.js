2548f0e72ba88d4935f1bb5048b8fef907734aef
Justin <justincrypto702@gmail.com>
2025-11-19 13:27:49 -0800

    Finish JavaScript exercise 09 sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+  for (let i=num1; i<=num2; i++) {
+    sum += i;
+  }
+  return sum;
 };
-
+console.log(sumAll(1, 4));
+//sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
 // Do not edit below this line
 module.exports = sumAll;
