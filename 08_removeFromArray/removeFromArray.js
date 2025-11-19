c68a65be7c98f08be9a7b3fa64a882d5a5536c56
Justin <justincrypto702@gmail.com>
2025-11-19 11:32:26 -0800

    Finish exercise 08 in JavaScript

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    if(typeof array === "object") {
+        return array;
+    }
 };
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
+//removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
