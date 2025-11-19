ab0c2aff5bff276e2181ca00530dada479b1b116
Justin <justincrypto702@gmail.com>
2025-11-19 11:22:48 -0800

    Complete JS ex 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+  return words.split("").reverse().join("") //without return, this would be discarded
 };
+console.log(reverseString("Hello World!"));
 
 // Do not edit below this line
 module.exports = reverseString;
