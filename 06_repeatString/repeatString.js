a9bed9f5a5d1ace7c40fbead5685cba974de0d5e
Justin <justincrypto702@gmail.com>
2025-11-19 11:03:59 -0800

    Finish ex 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberRepeats) {
+  let newArray = [];
+ for (let i=0; i<numberRepeats; i++){ //Loop pushes the next command this many times
+    newArray.push(string);
+ }
+ return newArray.join(""); //took away the space
 };
+console.log(repeatString("Hey", 3))
 
 // Do not edit below this line
 module.exports = repeatString;
