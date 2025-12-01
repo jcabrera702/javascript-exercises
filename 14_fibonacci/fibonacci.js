f24b50b4a821046803cea9a51a4fe0c76643fd79
Justin <justincrypto702@gmail.com>
2025-12-01 13:52:32 -0800

    Finish exercise 14 fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let newArray = [1, 1];
+  for(let i=2; i<=num; i++ ){
+   const nextFib = newArray[newArray.length-2] + newArray[newArray.length-1];
+    newArray.push(nextFib);
+  }
+  return newArray[num-1];
 };
+console.log(fibonacci(2));
 
 // Do not edit below this line
 module.exports = fibonacci;
