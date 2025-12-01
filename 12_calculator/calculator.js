01561ae7074eb8f066cbfc6a5c705557caf06ef0
Justin <justincrypto702@gmail.com>
2025-12-01 11:01:45 -0800

    Finished exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((a,b) => a + b, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a,b) => a*b);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0 || !Number.isInteger(num)){
+    return "Enter a positive number";
+  } else if (num === 0) {
+    return 1;
+  } else {
+    let result = 1
+    for (i=1; i<=num; i++){
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
