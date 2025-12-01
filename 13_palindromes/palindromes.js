f0b766eb71b1a5b412f657898090600d921eadd8
Justin <justincrypto702@gmail.com>
2025-12-01 13:10:28 -0800

    Finish palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+const sanitizedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+const reversedString = sanitizedString.split("").reverse().join("");
+return sanitizedString === reversedString;
 };
 
 // Do not edit below this line
