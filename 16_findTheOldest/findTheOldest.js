cf6af2de5d8b5ae593e12871dbd32aecd654ed25
Justin <justincrypto702@gmail.com>
2025-12-01 15:08:38 -0800

    Finish TOP object exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+const oldestPerson = array.reduce((a,b) =>{
+    if (a.yearOfBirth < b.yearOfBirth){
+        return a;
+    } else {
+        return b;
+    }
+})
+return oldestPerson.name;
 };
+console.log(findTheOldest(people));
+
+const greatestAge = function(array){
+    const longestLived = array.reduce((a,b) => {
+        if ((a.yearOfDeath - a.yearOfBirth)>(b.yearOfDeath-b.yearOfBirth)) {
+            return a;
+        } else {
+            return b;
+        }
+    });
+    return longestLived.name;
+}
+console.log(greatestAge(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
